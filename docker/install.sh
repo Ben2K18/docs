8cbf87ba63bc114b2e01525fa8b42e51367034c0
cnscn <yao.softs@gmail.com>
2017-05-30 19:09:13 +0800

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -1,15 +1,12 @@
 1) 安装docker
-sudo -s
-
-apt-get -y install apt-transport-https ca-certificates curl
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-apt-get update 
-
-apt-get -y install docker-ce
+$ apt-get update
+$ apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual 
+$ apt-get install apt-transport-https ca-certificates curl software-properties-common
+$ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+$ apt-key fingerprint 0EBFCD88
+$ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
+$ apt-get update
+$ apt-get install docker-ce
 
 2) 修改docker container的存储路径为/data/docker
    mv /var/lib/docker /data/docker
