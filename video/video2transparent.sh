d9ffe79705f24e43fb7869bfbde2e03bbbab6edd
cnscn <yao.softs@gmail.com>
2017-06-15 16:45:20 +0800

    Create video2transparent.sh

---
+++ video/video2transparent.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Convert mp4 to transparent webm
+# Date: 20170615
+
+#usage
+function usage() {
+   echo "Usage: $0 a.mp4 bgcolor color-relate-range"
+   echo "       $0 a.mp4 4fff87 10"
+   exit
+}
+
+#args
+[ $# -eq 3 ] || usage
+[ -f $1 ] || usage
+
+src=$1
+bgc=${2:-white}
+fuzz=${3:-30}
+fname=${src%.*}
+
+#install ffmpeg and imagemagick (for convert)
+[ -f /usr/bin/ffmpeg ]  || apt install ffmpeg
+[ -f /usr/bin/convert ] || apt install imagemagick 
+
+#convert directory
+[ -f /tmp/videotrans ] || mkdir /tmp/videotrans
+
+cp $src /tmp/videotrans
+cd /tmp/videotrans
+
+[ -d png ] || mkdir png ; [ -d mp3 ] || mkdir mp3 ; rm -f png/* ; rm -f mp3/*
+
+#extract audio from video
+ffmpeg -i ab.mp4 -q:a 0 -map a mp3/${fname}.mp3
+
+#extract png from video
+ffmpeg -i $src -f image2 png/2\%d.png
+
+#get rate from video
+rate=$(ffmpeg -i ab.mp4 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p")
+
+#png to transparent
+cd png
+for f in $(ls *.png)
+do
+   convert $f -channel rgba -alpha set -fuzz ${fuzz}% -fill none -opaque "#${bgc}" trans-${f}
+   rm -f $f
+done
+cd ..
+
+#combine png and mp3 to webm
+rm -f output.webm
+yes|ffmpeg -i mp3/${fname}.mp3 -framerate $rate -f image2 -i png/trans-2\%d.png -c:v libvpx -pix_fmt yuva420p ${fname}.webm
+
+rm -f png/*
+rm -f mp3/*
+
+#End of Script
