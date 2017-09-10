48ce108d3b739120087430d6199e0f15b336485f
cnscn <yao.softs@gmail.com>
2017-09-10 20:58:12 +0800

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -56,36 +56,44 @@ $ apt-get install docker-ce
    3) restart
    /etc/init.d/ssh restart
    
-6) 添加端口映射
+6) 添加端口映射, 如映射host:8080 ==>  myubuntu:80
 #!/bin/bash
-# Script: portmap.sh
-# Usage: <b>./portmap.sh</b>  8080  <b>myubuntu</b> 80
+if [ $# -ne 3 ]
+then
+    echo Usage: $0 host_port comtainer_port
+    echo        $0 myubuntu 8080 80
+    exit
+fi
 
-[ $# -eq 3 ] || { 
-   echo "Usage: $0 host_port containerid container_port";
-   echo "       $0 85 myserver 80"; 
-   exit 1; 
-}
-  
-function portmap() {
-   host_port=$1
-   containerid=$2
-   container_port=$3
+container=$1
+sport=$2
+dport=$3
 
-   ip=$(docker inspect webserver2 | grep '\<IPAddress\>' | tail -1 | sed 's/"\|,\| //g' | awk -F: '{print $2}')    
-   
-   /sbin/iptables -A DOCKER -d $ip/32 ! -i docker0 -o docker0 -p tcp -m tcp --dport $host_port -j ACCEPT
-   /sbin/iptables -t nat  -A POSTROUTING -s $ip/32 -d $ip/32 -p tcp -m tcp --dport $host_port -j MASQUERADE
-   /sbin/iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport $host_port -j DNAT --to-destination $ip:$container_port
+times=0
+while true
+do
+  docker ps | grep $container
+  if [ $? -eq 0 ]
+  then
+     break
+ fi
 
-   #/sbin/iptables -A DOCKER -d 172.17.0.2/32 ! -i docker0 -o docker0 -p tcp -m tcp --dport 443 -j ACCEPT
-   #/sbin/iptables -t nat  -A POSTROUTING -s 172.17.0.2/32 -d 172.17.0.2/32 -p tcp -m tcp --dport 443 -j MASQUERADE
-   #/sbin/iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport 443 -j DNAT --to-destination 172.17.0.2:443
-}
+  if [ $times -gt 100 ]
+  then
+      echo $container not  running >> /var/log/syslog
+    exit
+  fi
+  sleep 3
+  ((times=times+1))
+done
 
-portmap $1 $2 $3
+dip=$(docker inspect $container | grep '\<IPAddress\>' | tail -1 | sed 's/"\|,\| //g' | awk -F: '{print $2}')
 
-ufw allow $1
+/sbin/iptables -t nat -A DOCKER -p tcp --dport $sport -j DNAT --to-destination $dip:$dport
+/sbin/iptables -t nat -A POSTROUTING -j MASQUERADE -p tcp --source $dip --destination $dip --dport $dport
+/sbin/iptables -A DOCKER -j ACCEPT -p tcp --destination $dip --dport $dport
+
+ufw allow $sport
 #End of Script
 
 7) 修改hostname
