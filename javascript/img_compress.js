fb5d006407769dcdff0d9e650c2bddac74110d2e
cnscn <yao.softs@gmail.com>
2017-09-21 00:39:39 +0800

    Create img_compress.js

---
+++ javascript/img_compress.js
@@ -0,0 +1,25 @@
+function compress(source_img_obj, quality, maxWidth, output_format){
+    var mime_type = "image/jpeg";
+    if(typeof output_format !== "undefined" && output_format=="png"){
+        mime_type = "image/png";
+    }
+
+    maxWidth = maxWidth || 1000;
+    var natW = source_img_obj.naturalWidth;
+    var natH = source_img_obj.naturalHeight;
+    var ratio = natH / natW;
+    if (natW > maxWidth) {
+        natW = maxWidth;
+        natH = ratio * maxWidth;
+    }
+
+    var cvs = document.createElement('canvas');
+    cvs.width = natW;
+    cvs.height = natH;
+
+    var ctx = cvs.getContext("2d").drawImage(source_img_obj, 0, 0, natW, natH);
+    var newImageData = cvs.toDataURL(mime_type, quality/100);
+    var result_image_obj = new Image();
+    result_image_obj.src = newImageData;
+    return result_image_obj;
+}
