4c32df915028982d19b289b9dfe1a068d370768a
cnscn <yao.softs@gmail.com>
2017-11-12 10:28:36 +0800

    Update ssh.login.attack.sh

--- LNMP/ssh.login.attack.sh
+++ LNMP/ssh.login.attack.sh
@@ -10,11 +10,11 @@ do
    read num ip _ <<< "$line"
    if [ $num -gt  5 ]
    then
-     ufw status |grep $ip >/dev/null
+     /usr/sbin/ufw status |grep $ip >/dev/null
      if [ $? -ne 0 ]
      then
         echo -n "deny from $ip "
-        ufw insert 1 deny from $ip
+        /usr/sbin/ufw insert 1 deny from $ip
      fi
    fi
 done <<< "$(lastb |awk '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]/ {print $3,$4,$5,$6,$7}'|grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' |awk -F: '{print $1}'|sort|uniq -c|sort -nr)"
