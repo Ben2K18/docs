015827913b119262ea1ba7c970e571d5742ae930
cnscn <yao.softs@gmail.com>
2018-01-05 14:59:54 +0800

    Update Thread.java

--- Java/Thread.java
+++ Java/Thread.java
@@ -1,4 +1,12 @@
-      (new Thread(new Runnable() {
+      //1)
+      (new Thread() {
+          public void run() {
+              System.out.println("thread here...");
+          }
+      }).start();
+
+     //2) Runnable
+     (new Thread(new Runnable() {
             public void run() {
                 System.out.println("thread here...");
             }
