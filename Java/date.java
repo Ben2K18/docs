86687fe2d85dad70ad002975dce2cb1cc998805e
cnscn <yao.softs@gmail.com>
2018-01-05 16:54:19 +0800

    Update date.java

--- Java/date.java
+++ Java/date.java
@@ -3,3 +3,7 @@
 //ft.format(new Date())
 
 System.out.println("Current Date: " + (new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz")).format(new Date()));
+
+String str = String.format("Current Date/Time : %tc", (new Date()) );
+
+System.out.printf("%1$s %2$tB %2$td, %2$tY", "Due date:", (new Date()));
