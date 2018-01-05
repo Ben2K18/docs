a341bd2e39c8a7910b40a161d3b2c55008f38d7b
cnscn <yao.softs@gmail.com>
2018-01-05 14:45:24 +0800

    Create ThreadArgs.java

---
+++ Java/ThreadArgs.java
@@ -0,0 +1,17 @@
+import java.utils.*;
+
+class ThreadArgs implements Runnable {    
+    private volatile String str;
+
+    //传参
+    public ThreadArgs(String str){
+        this.str = str;
+    }
+
+    public void run(){
+        System.out.println(str);
+    }
+}
+
+//传参运行 
+new Thread(new ThreadArgs("Hello World")).start();
