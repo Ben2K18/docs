32c28aa52ea9bec26155d1cdd446771531094683
cnscn <yao.softs@gmail.com>
2018-01-05 15:15:10 +0800

    Update A.java

--- Java/A.java
+++ Java/A.java
@@ -1,5 +1,10 @@
+//1) 文件名: 必须是 类名.java
+//2) 类名:   首字母大写
+//3) 方法名: 首字母小写 getName() getNickName()
+
+
 public class A {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         System.out.println("Hello World");
     }
 }
