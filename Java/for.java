1b6cc6b63a7246eaaae5b7a72d06d8ba4de27eab
cnscn <yao.softs@gmail.com>
2018-01-05 16:47:20 +0800

    Update for.java

--- Java/for.java
+++ Java/for.java
@@ -23,3 +23,10 @@ for(char c : chars) {
     System.out.println(""+c);
 
 }
+
+4)
+double[] myList = {1.9, 2.9, 3.4, 3.5};
+
+for (int i = 0; i < myList.length; i++) {
+     System.out.println(myList[i] + " ");
+}
