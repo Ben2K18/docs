e9938f165c9c2144c98815e5f5b46a3f6d75bd78
cnscn <yao.softs@gmail.com>
2018-01-05 15:55:31 +0800

    Update enum.java

--- Java/enum.java
+++ Java/enum.java
@@ -1,47 +1,51 @@
-//定义
+//定义 (必须在类外面，否则编译报错)
 enum MySize { Small, Medium, Large }
 enum WeekDay { Mon, Tue, Wed, Thu, Fri, Sat, Sun }
 
-//定义变量
-WeekDay today=WeekDay.Mon; 
+public class A {
+    public static void main(String args[]) {
+        //定义变量
+        WeekDay today=WeekDay.Mon; 
 
-//输出
-System.out.println("WeekDay: "+((int)today)+" "+today.toString()); // Mon Mon
+        //输出
+        System.out.println("WeekDay: "+(today)+" "+today.toString()); // Mon Mon
 
-//if
-if(today == WeekDay.Mon)
-    System.out.println("Mon2");
+        //if
+        if(today == WeekDay.Mon)
+            System.out.println("Mon2");
 
-//switch
-switch (today) {
-    //Error: case "Mon":
-    //Error: case WeekDay.Mon
-    //OK: Mon
-    case Mon:
-        System.out.println("Mon");
-        break;
-        
-    case Tue:
-        System.out.println("Tue");
-         break;
-         
-    case Wed:
-         System.out.println("Wed");
-         break;
-         
-    case Thu:
-         System.out.println("Thu");
-         break;
-         
-    case Fri:
-         System.out.println("Fri");
-         break;
-         
-    case Sat:
-         System.out.println("Sat");
-         break;
-         
-    case Sun:
-         System.out.println("Sun");
-         break;
+        //switch
+        switch (today) {
+            //Error: case "Mon":
+            //Error: case WeekDay.Mon
+            //OK: Mon
+            case Mon:
+                System.out.println("Mon");
+                break;
+
+            case Tue:
+                System.out.println("Tue");
+                break;
+
+            case Wed:
+                System.out.println("Wed");
+                break;
+
+            case Thu:
+                System.out.println("Thu");
+                break;
+
+            case Fri:
+                System.out.println("Fri");
+                break;
+
+            case Sat:
+                System.out.println("Sat");
+                break;
+
+            case Sun:
+                System.out.println("Sun");
+                break;
+        }
+    }
 }
