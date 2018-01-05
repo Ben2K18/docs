a5a6a50e72c4b043c462873ca1028805a1b72b16
cnscn <yao.softs@gmail.com>
2018-01-05 16:51:27 +0800

    Create date.java

---
+++ Java/date.java
@@ -0,0 +1,5 @@
+//Date dNow = new Date();
+//SimpleDateFormat ft = new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+//ft.format(new Date())
+
+System.out.println("Current Date: " + (new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz")).format(new Date()));
