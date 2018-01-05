e00093702fe035229452c28bfefe02078aa2c9fa
cnscn <yao.softs@gmail.com>
2018-01-05 16:46:33 +0800

    Create String.java

---
+++ Java/String.java
@@ -0,0 +1,8 @@
+String str = "Dot saw I was Tod";
+int len = str.length();
+System.out.println( "String Length is : " + len );
+
+System.out.printf("variable is %f, %d, %s", floatVar, intVar, stringVar);
+
+String fs = ("variable is %f, %d, %s", floatVar, intVar, stringVar);
+System.out.println(fs);
