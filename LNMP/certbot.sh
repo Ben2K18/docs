f8cef4da69418e4a4bcabfce22af0ce086a480d7
cnscn <yao.softs@gmail.com>
2018-01-16 21:44:41 +0800

    Update certbot.sh

--- LNMP/certbot.sh
+++ LNMP/certbot.sh
@@ -1,9 +1,14 @@
  add-apt-repository ppa:certbot/certbot 
  apt-get update 
  apt-get install python-certbot-nginx
-
- 安装证书
- certbot --nginx
  
- 更新证书
- certbot renew
+.安装证书 
+#!/bin/bash
+# certbot --nginx
+certbot --authenticator standalone --installer nginx --pre-hook "service nginx stop" --post-hook "service nginx start"
+
+
+.更新证书
+#!/bin/bash
+# certbot renew
+certbot --authenticator standalone --installer nginx --pre-hook "service nginx stop" --post-hook "service nginx start" renew
